fc29425abe99e26d2e5ba0ee6c5c8c7c736ca3ed
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2026-02-08 13:52:38 -0500

    svelte scafold

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>NOPAL</title>
+	<meta name="description" content="A very sophisticated temperature conversion tool." />
+</svelte:head>
+{@render children()}
